5f6e4b70edd37a59742d2deca10954781ea03dee
Rylan Collins <rylan@gusto.com>
2019-07-08 12:52:10 -0700

    Add description to the _Service type

--- lib/apollo-federation/service.rb
+++ lib/apollo-federation/service.rb
@@ -5,6 +5,9 @@ require 'graphql'
 module ApolloFederation
   class Service < GraphQL::Schema::Object
     graphql_name '_Service'
+    description 'The sdl representing the federated service capabilities. Includes federation ' \
+      'directives, removes federation types, and includes rest of full schema after schema ' \
+      'directives have been applied'
 
     field(:sdl, String, null: true)
   end
