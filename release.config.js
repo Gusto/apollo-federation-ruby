d88271a9a728908f120d6fca726b219582747535
Rylan Collins <rylan@gusto.com>
2019-08-02 11:04:12 -0700

    Add changelog plugin

--- release.config.js
+++ release.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    // '@semantic-release/release-notes-generator',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     [
       '@semantic-release/exec',
       {
@@ -13,6 +14,14 @@ module.exports = {
           'ruby bin/publish.rb ${nextRelease.version}',
       },
     ],
+    // [
+    //   '@semantic-release/git',
+    //   {
+    //     // eslint-disable-next-line no-template-curly-in-string
+    //     message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    //     assets: ['CHANGELOG.md', ],
+    //   },
+    // ],
     // '@semantic-release/github',
   ],
 };
