07f8d03d234ec7a177b0a2ea1234ae9bd2da1436
Rylan Collins <rylan@gusto.com>
2019-08-02 11:17:28 -0700

    Delete built gem after publish

--- bin/publish.rb
+++ bin/publish.rb
@@ -2,6 +2,8 @@
 
 # TODO: Rewrite in bash?
 new_version = ARGV[0]
-puts("gem push apollo-federation-#{new_version}.gem")
+gem_name = "apollo-federation-#{new_version}.gem"
+puts("gem push #{gem_name}")
+File.delete(gem_name)
 # system("gem push apollo-federation-#{new_version}.gem")
 # puts('{}')
