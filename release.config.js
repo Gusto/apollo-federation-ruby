fe68c4137d288dcd3b20283233db5a4f967de15b
Rylan Collins <rylan@gusto.com>
2019-08-02 11:21:51 -0700

    Add git plugin

--- release.config.js
+++ release.config.js
@@ -14,14 +14,14 @@ module.exports = {
           'ruby bin/publish.rb ${nextRelease.version}',
       },
     ],
-    // [
-    //   '@semantic-release/git',
-    //   {
-    //     // eslint-disable-next-line no-template-curly-in-string
-    //     message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    //     assets: ['CHANGELOG.md', ],
-    //   },
-    // ],
+    [
+      '@semantic-release/git',
+      {
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        assets: ['CHANGELOG.md', 'Gemfile.lock', 'lib/apollo-federation/version.rb'],
+      },
+    ],
     // '@semantic-release/github',
   ],
 };
