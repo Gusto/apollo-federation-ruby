2e1ec697b6b4c60d6d49a9a1df9050aa65f6ff2a
Hector Medina-Fetterman <javi@digitalhospital.com>
2020-02-20 03:24:53 +0100

    Update schema.rb
    Pulling out some code from to_graphql to meet the complexity checks

--- lib/apollo-federation/schema.rb
+++ lib/apollo-federation/schema.rb
@@ -15,6 +15,26 @@ module ApolloFederation
       def to_graphql
         orig_defn = super
 
+        possible_entities = orig_defn.types.values.select do |type|
+          !type.introspection? && !type.default_scalar? && type.is_a?(GraphQL::ObjectType) &&
+            type.metadata[:federation_directives]&.any? { |directive| directive[:name] == 'key' }
+        end
+
+        @query_object = federation_query
+
+        if !possible_entities.empty?
+          entity_type = Class.new(Entity) do
+            possible_types(*possible_entities)
+          end
+          # TODO: Should/can we encapsulate all of this inside the module? What's the best/most Ruby
+          # way to split this out?
+          @query_object.define_entities_field(entity_type)
+        end
+
+        super
+      end
+
+      def federation_query
         if query.nil?
           base = GraphQL::Schema::Object
         elsif Gem::Version.new(GraphQL::VERSION) >= Gem::Version.new('1.10.0')
@@ -23,30 +43,12 @@ module ApolloFederation
           base = query.metadata[:type_class]
         end
 
-        federation_query = Class.new(base) do
+        Class.new(base) do
           graphql_name 'Query'
 
           include EntitiesField
           include ServiceField
         end
-
-        possible_entities = orig_defn.types.values.select do |type|
-          !type.introspection? && !type.default_scalar? && type.is_a?(GraphQL::ObjectType) &&
-            type.metadata[:federation_directives]&.any? { |directive| directive[:name] == 'key' }
-        end
-
-        if !possible_entities.empty?
-          entity_type = Class.new(Entity) do
-            possible_types(*possible_entities)
-          end
-          # TODO: Should/can we encapsulate all of this inside the module? What's the best/most Ruby
-          # way to split this out?
-          federation_query.define_entities_field(entity_type)
-        end
-
-        @query_object = federation_query
-
-        super
       end
 
       def federation_sdl(context: nil)
