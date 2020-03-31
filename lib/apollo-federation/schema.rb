6bdef68074092eada963eeb2a97450c67b788d75
Hector Medina-Fetterman <javi@digitalhospital.com>
2020-03-31 17:45:02 +0200

    chore(deps): Allow for GraphQL dependencies >= 1.10.0 (#49)
    * chore(deps): Allow for GraphQL dependencies >= 1.10.0
    
    graphql-ruby 1.10.0 brought some breaking changes to its to_graphql method. See: https://github.com/rmosolgo/graphql-ruby/blob/master/CHANGELOG.md#1100-20-jan-2020
    
    * Update schema.rb
    
    Fixing some lint errors
    
    * Update schema.rb
    
    Pulling out some code from to_graphql to meet the complexity checks

--- lib/apollo-federation/schema.rb
+++ lib/apollo-federation/schema.rb
@@ -15,23 +15,12 @@ module ApolloFederation
       def to_graphql
         orig_defn = super
 
-        if query.nil?
-          base = GraphQL::Schema::Object
-        else
-          base = query.metadata[:type_class]
-        end
-
-        federation_query = Class.new(base) do
-          graphql_name 'Query'
-
-          include EntitiesField
-          include ServiceField
-        end
-
         possible_entities = orig_defn.types.values.select do |type|
           !type.introspection? && !type.default_scalar? && type.is_a?(GraphQL::ObjectType) &&
             type.metadata[:federation_directives]&.any? { |directive| directive[:name] == 'key' }
         end
+
+        @query_object = federation_query
 
         if !possible_entities.empty?
           entity_type = Class.new(Entity) do
@@ -39,12 +28,27 @@ module ApolloFederation
           end
           # TODO: Should/can we encapsulate all of this inside the module? What's the best/most Ruby
           # way to split this out?
-          federation_query.define_entities_field(entity_type)
+          @query_object.define_entities_field(entity_type)
         end
 
-        query(federation_query)
-
         super
+      end
+
+      def federation_query
+        if query.nil?
+          base = GraphQL::Schema::Object
+        elsif Gem::Version.new(GraphQL::VERSION) >= Gem::Version.new('1.10.0')
+          base = query
+        else
+          base = query.metadata[:type_class]
+        end
+
+        Class.new(base) do
+          graphql_name 'Query'
+
+          include EntitiesField
+          include ServiceField
+        end
       end
 
       def federation_sdl(context: nil)
