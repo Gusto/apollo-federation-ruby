a44379879f7251a1252332d752bc001727975a5a
Rylan Collins <rylan@gusto.com>
2020-06-22 12:10:07 -0700

    fix: Testing

--- release.config.js
+++ release.config.js
@@ -1,27 +1,28 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/exec',
-      {
-        // TODO: Verify the presence of the GEM_HOST_API_KEY env var
-        // verifyConditionsCmd: './verify.sh',
-        // eslint-disable-next-line no-template-curly-in-string
-        prepareCmd: 'ruby bin/prepare.rb ${nextRelease.version}',
-        // eslint-disable-next-line no-template-curly-in-string
-        publishCmd: 'ruby bin/publish.rb ${nextRelease.version}',
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        // eslint-disable-next-line no-template-curly-in-string
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        assets: ['CHANGELOG.md', '**/*.lock', 'lib/apollo-federation/version.rb'],
-      },
-    ],
-    '@semantic-release/github',
+    // '@semantic-release/release-notes-generator',
+    // '@semantic-release/changelog',
+    'semantic-release-rubygem',
+    // [
+    //   '@semantic-release/exec',
+    //   {
+    //     // TODO: Verify the presence of the GEM_HOST_API_KEY env var
+    //     // verifyConditionsCmd: './verify.sh',
+    //     // eslint-disable-next-line no-template-curly-in-string
+    //     prepareCmd: 'ruby bin/prepare.rb ${nextRelease.version}',
+    //     // eslint-disable-next-line no-template-curly-in-string
+    //     publishCmd: 'ruby bin/publish.rb ${nextRelease.version}',
+    //   },
+    // ],
+    // [
+    //   '@semantic-release/git',
+    //   {
+    //     // eslint-disable-next-line no-template-curly-in-string
+    //     message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    //     assets: ['CHANGELOG.md', '**/*.lock', 'lib/apollo-federation/version.rb'],
+    //   },
+    // ],
+    // '@semantic-release/github',
   ],
 };
