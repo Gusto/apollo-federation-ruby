e1be827da779877ca0580fc16977fdf1aebdc57a
Rylan Collins <rylan@gusto.com>
2020-07-16 13:45:48 -0700

    build: Use semantic-release-rubygem for publishing

--- bin/publish.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-new_version = ARGV[0]
-gem_name = "apollo-federation-#{new_version}.gem"
-system("gem push #{gem_name}")
-File.delete(gem_name)
--- release.config.js
+++ release.config.js
@@ -1,29 +1,22 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    // '@semantic-release/release-notes-generator',
-    // '@semantic-release/changelog',
-    'semantic-release-rubygem',
-    // [
-    //   '@semantic-release/exec',
-    //   {
-    //     // TODO: Verify the presence of the GEM_HOST_API_KEY env var
-    //     // verifyConditionsCmd: './verify.sh',
-    //     // eslint-disable-next-line no-template-curly-in-string
-    //     prepareCmd: 'ruby bin/prepare.rb ${nextRelease.version}',
-    //     // eslint-disable-next-line no-template-curly-in-string
-    //     publishCmd: 'ruby bin/publish.rb ${nextRelease.version}',
-    //   },
-    // ],
-    // [
-    //   '@semantic-release/git',
-    //   {
-    //     // eslint-disable-next-line no-template-curly-in-string
-    //     message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    //     assets: ['CHANGELOG.md', '**/*.lock', 'lib/apollo-federation/version.rb'],
-    //   },
-    // ],
-    // '@semantic-release/github',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      'semantic-release-rubygem',
+      {
+        updateGemfileLock: 'bundle exec appraisal install',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        assets: ['CHANGELOG.md', '**/*.lock', 'lib/apollo-federation/version.rb'],
+      },
+    ],
+    '@semantic-release/github',
   ],
-  branches: 'rc-semantic-release-rubygem'
 };
