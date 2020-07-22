d3edc8d40bef18c89f8fa792d6b495f831075c22
Rylan Collins <rylan@gusto.com>
2020-07-22 14:13:57 -0700

    build: Use semantic-release-rubygem for publishing (#89)
    * fix: Testing
    
    * Use this branch
    
    * build: Use semantic-release-rubygem for publishing
    
    * chore: bump semantic-release-rubygem
    
    * chore: bump semantic-release-rubygem@1.1.2

--- release.config.js
+++ release.config.js
@@ -4,14 +4,9 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
-      '@semantic-release/exec',
+      'semantic-release-rubygem',
       {
-        // TODO: Verify the presence of the GEM_HOST_API_KEY env var
-        // verifyConditionsCmd: './verify.sh',
-        // eslint-disable-next-line no-template-curly-in-string
-        prepareCmd: 'ruby bin/prepare.rb ${nextRelease.version}',
-        // eslint-disable-next-line no-template-curly-in-string
-        publishCmd: 'ruby bin/publish.rb ${nextRelease.version}',
+        updateGemfileLock: 'bundle exec appraisal install',
       },
     ],
     [
