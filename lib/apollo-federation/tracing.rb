6cf820281dd85bd358c6bf4c176b9a73a9280d54
Rylan Collins <rylan@gusto.com>
2020-09-21 12:59:17 -0700

    fix(tracing): properly handle parsing and validation errors (#101)
    * fix(tracing): properly handle parsing and validation errors
    
    * style: fix linter errors
    
    * style: fix linter errors

--- lib/apollo-federation/tracing.rb
+++ lib/apollo-federation/tracing.rb
@@ -4,14 +4,6 @@ module ApolloFederation
   module Tracing
     KEY = :ftv1
     DEBUG_KEY = "#{KEY}_debug".to_sym
-    class NotInstalledError < StandardError
-      MESSAGE = 'Apollo Federation Tracing not installed. \
-Add `use ApolloFederation::Tracing` to your schema.'
-
-      def message
-        MESSAGE
-      end
-    end
 
     module_function
 
@@ -23,37 +15,10 @@ Add `use ApolloFederation::Tracing` to your schema.'
       headers && headers['apollo-federation-include-trace'] == KEY.to_s
     end
 
-    def attach_trace_to_result(result)
-      return result unless result.context[:tracing_enabled]
-
-      trace = result.context.namespace(KEY)
-      # TODO: If there are errors during query validation, that could also cause a missing
-      # start_time
-      raise NotInstalledError unless trace[:start_time]
-
-      result['errors']&.each do |error|
-        trace[:node_map].add_error(error)
-      end
-
-      proto = ApolloFederation::Tracing::Trace.new(
-        start_time: to_proto_timestamp(trace[:start_time]),
-        end_time: to_proto_timestamp(trace[:end_time]),
-        duration_ns: trace[:end_time_nanos] - trace[:start_time_nanos],
-        root: trace[:node_map].root,
-      )
-
-      result[:extensions] ||= {}
-      result[:extensions][KEY] = Base64.encode64(proto.class.encode(proto))
-
-      if result.context[:debug_tracing]
-        result[:extensions][DEBUG_KEY] = proto.to_h
-      end
-
-      result.to_h
-    end
-
-    def to_proto_timestamp(time)
-      Google::Protobuf::Timestamp.new(seconds: time.to_i, nanos: time.nsec)
+    # @deprecated There is no need to call this method. Traces are added to the result automatically
+    def attach_trace_to_result(_result)
+      warn '[DEPRECATION] `attach_trace_to_result` is deprecated. There is no need to call it, as '\
+        'traces are added to the result automatically'
     end
   end
 end
