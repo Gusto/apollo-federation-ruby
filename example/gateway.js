cd53e9549d605adeace86e84396ddb842d6d1453
Austin Story <lonnieastory@gmail.com>
2021-12-09 14:04:40 -0600

    Comment out so that we only have the products subgraph

--- example/gateway.js
+++ example/gateway.js
@@ -3,18 +3,20 @@ const { ApolloGateway } = require('@apollo/gateway');
 
 const gateway = new ApolloGateway({
   serviceList: [
-    { name: 'accounts', url: 'http://localhost:5001/graphql' },
-    { name: 'reviews', url: 'http://localhost:5002/graphql' },
+    // { name: 'accounts', url: 'http://localhost:5001/graphql' },
+    // { name: 'reviews', url: 'http://localhost:5002/graphql' },
     { name: 'products', url: 'http://localhost:5003/graphql' },
-    { name: 'inventory', url: 'http://localhost:5004/graphql' },
+    // { name: 'inventory', url: 'http://localhost:5004/graphql' },
   ],
   debug: true,
 });
 
-(async () => {
+func = (async () => {
   const server = new ApolloServer({ gateway, subscriptions: false });
 
   server.listen({ port: 5000 }).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
   });
-})();
+})
+setTimeout(func, 1500)
+
