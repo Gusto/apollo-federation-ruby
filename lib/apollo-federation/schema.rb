4fe11a6ac6cfb2ae28e57ccb1525fcb17c6a03c2
Damon Aw <damon.aw@gusto.com>
2022-01-26 12:09:51 -0500

    Remove support for GraphQL 1.9.x

--- lib/apollo-federation/schema.rb
+++ lib/apollo-federation/schema.rb
@@ -8,11 +8,7 @@ require 'apollo-federation/federated_document_from_schema_definition.rb'
 module ApolloFederation
   module Schema
     def self.included(klass)
-      if Gem::Version.new(GraphQL::VERSION) >= Gem::Version.new('1.10.0')
-        klass.extend(OneTenMethods)
-      else
-        klass.extend(OneNineMethods)
-      end
+      klass.extend(CommonMethods)
     end
 
     module CommonMethods
@@ -20,53 +16,6 @@ module ApolloFederation
         document_from_schema = FederatedDocumentFromSchemaDefinition.new(self, context: context)
         GraphQL::Language::Printer.new.print(document_from_schema.document)
       end
-
-      private
-
-      def federation_query(query_obj)
-        # Build the new query object with the '_service' field
-        if query_obj.nil?
-          base = GraphQL::Schema::Object
-        elsif Gem::Version.new(GraphQL::VERSION) >= Gem::Version.new('1.10.0')
-          base = query_obj
-        else
-          base = query_obj.metadata[:type_class]
-        end
-
-        klass = Class.new(base) do
-          # TODO: Maybe the name should inherit from the original Query name
-          # Or MAYBE we should just modify the original class?
-          graphql_name 'Query'
-
-          include EntitiesField
-          include ServiceField
-        end
-
-        klass.define_service_field
-        klass
-      end
-    end
-
-    # TODO: Remove these once we drop support for graphql 1.9
-    module OneNineMethods
-      include CommonMethods
-
-      def to_graphql
-        orig_defn = super
-        @query_object = federation_query(query)
-
-        possible_entities = orig_defn.types.values.select do |type|
-          !type.introspection? && !type.default_scalar? && type.is_a?(GraphQL::ObjectType) &&
-            type.metadata[:federation_directives]&.any? { |directive| directive[:name] == 'key' }
-        end
-        @query_object.define_entities_field(possible_entities)
-
-        super
-      end
-    end
-
-    module OneTenMethods
-      include CommonMethods
 
       def query(new_query_object = nil)
         if new_query_object
@@ -99,6 +48,29 @@ module ApolloFederation
           !type.introspection? && type.include?(ApolloFederation::Object) &&
             type.federation_directives&.any? { |directive| directive[:name] == 'key' }
         end
+      end
+
+      def federation_query(query_obj)
+        # Build the new query object with the '_service' field
+        if query_obj.nil?
+          base = GraphQL::Schema::Object
+        elsif Gem::Version.new(GraphQL::VERSION) >= Gem::Version.new('1.10.0')
+          base = query_obj
+        else
+          base = query_obj.metadata[:type_class]
+        end
+
+        klass = Class.new(base) do
+          # TODO: Maybe the name should inherit from the original Query name
+          # Or MAYBE we should just modify the original class?
+          graphql_name 'Query'
+
+          include EntitiesField
+          include ServiceField
+        end
+
+        klass.define_service_field
+        klass
       end
     end
   end
