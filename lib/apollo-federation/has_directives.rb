c700961d852e6f73f86e1e2458cb789067d4c8c9
Damon Aw <damon.aw@gusto.com>
2022-01-31 10:22:10 -0500

    This should work without passing args?

--- lib/apollo-federation/has_directives.rb
+++ lib/apollo-federation/has_directives.rb
@@ -9,7 +9,7 @@ module ApolloFederation
       @federation_directives << { name: name, arguments: arguments }
     end
 
-    def to_graphql(*args, **kwargs)
+    def to_graphql
       field_defn = super # Returns a GraphQL::Field
       field_defn.metadata[:federation_directives] = @federation_directives
       field_defn
