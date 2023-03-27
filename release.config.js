94c2c317b7beaade491654bbd9149153f9a0b87a
Grex <andrew@grexa.me>
2023-03-27 10:37:59 -0600

    chore: release a patch for all chore commits

--- release.config.js
+++ release.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [{ type: 'chore', release: 'patch' }],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
