d4216d606bb6168bf119b060d57c13376789e27a
Alex Vlasov <palexvs@gmail.com>
2024-08-13 14:01:10 -0700

    Silence Grphql Tracing deprecation warning

--- lib/apollo-federation/tracing.rb
+++ lib/apollo-federation/tracing.rb
@@ -9,11 +9,22 @@ module ApolloFederation
     module_function
 
     def use(schema)
-      schema.tracer ApolloFederation::Tracing::Tracer
+      if silence_deprecation_warning?
+        schema.tracer ApolloFederation::Tracing::Tracer, silence_deprecation_warning: true
+      else
+        schema.tracer ApolloFederation::Tracing::Tracer
+      end
     end
 
     def should_add_traces(headers)
       headers && headers[HEADER_NAME] == KEY.to_s
+    end
+
+    # Tracing is depreacted in graphql-ruby 2.0.0 and will be removed in 3.0.0
+    # https://github.com/rmosolgo/graphql-ruby/pull/4878/files#
+    def silence_deprecation_warning?
+      graphql_version = Gem.loaded_specs['graphql'].version
+      graphql_version >= Gem::Version.new('2') && graphql_version < Gem::Version.new('3')
     end
 
     # @deprecated There is no need to call this method. Traces are added to the result automatically
