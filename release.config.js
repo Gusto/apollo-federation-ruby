2e935a9a6128927ad57eb3cad5ca0739696ebe2c
Seth Copeland <seth.copeland@gusto.com>
2025-10-17 12:28:07 -0700

    chore: use token to push semantic release commit (#304)
    * Semantic release token.
    
    * Ignore lint error.
    
    * Use github token with correct permissions.

--- release.config.js
+++ release.config.js
@@ -24,6 +24,8 @@ module.exports = {
           'Gemfile.lock',
           'lib/apollo-federation/version.rb',
         ],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
     '@semantic-release/github',
